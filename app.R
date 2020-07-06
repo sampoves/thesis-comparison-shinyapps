fe6220fdda9d9ab732149ecbdcd11c91cfff1fa0
Sampo Vesanen <31888573+sampoves@users.noreply.github.com>
2020-07-07 01:50:46 +0300

    update strings and tooltip updating
    - an attempt to enable scrolling of tooltip in small screens. this should work for mouse wheels, and maybe for touch screens: needs testing (tested: unsuccessful)
    - try to make the message of strings clearer

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 # Helsinki Region Travel Time comparison application
 # Helsinki Region Travel Time Matrix 2018 <--> My thesis survey results
 
-# 5.7.2020
+# 7.7.2020
 # Sampo Vesanen
 
 
@@ -30,7 +30,7 @@ library(rlang)
 
 
 # App version
-app_v <- "0059.postal (5.7.2020)"
+app_v <- "0060.postal (7.7.2020)"
 
 # Data directories
 munspath <- "appdata/hcr_muns.shp"
